a155d8fe3fc6a78b4f5fa4b53c4a08f03061a1cf
gauravgandhigg <gaurav.gandhi15698@gmail.com>
2020-04-10 15:40:33 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//if user clicks on button and there is text there add it to the list
+
+document.getElementById('add').addEventListener('click',function(){
+    var value=document.getElementById('item').value;
+    if(value){
+        
+    }
+    
+    console.log(value); 
+
+});
